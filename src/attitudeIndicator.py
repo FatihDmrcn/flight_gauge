659e3b61393f8936445514ad20b2f988876542fa
FatihDmrcn <27896339+FatihDmrcn@users.noreply.github.com>
2021-06-05 17:24:39 +0200

    Paint event restructured

--- src/attitudeIndicator.py
+++ src/attitudeIndicator.py
@@ -8,178 +8,183 @@ import PyQt5.QtCore as qtc
 import PyQt5.QtGui as qtg
 import numpy as np
 
+
 class PrimaryFlightDisplay(qtw.QWidget):
+
+    blue = qtg.QColor(0, 64, 128, 255)
+    brown = qtg.QColor(120, 64, 0, 255)
+    white = qtc.Qt.white
+
     def __init__(self, size, *args, **kwargs):
         super(PrimaryFlightDisplay, self).__init__(*args, **kwargs)
         if size<300:size=300
-        self.setFixedSize((4/3)*size,size)
+        self.setFixedSize(int((4/3)*size), size)
         self.unit = self.height()/100
-        #Scaling
+        # Scaling
         self.rollScale = 0.9
         self.aircraftScale = 0.6
         self.pitchPadding = 1.5
-        #Font
+        # Font
         self.font = qtg.QFont("Courier New")
         self.font.setWeight(61)
-        self.font.setPixelSize(5*self.unit)
+        self.font.setPixelSize(int(5*self.unit))
         self.setFont(self.font)
-        #Values
-        self.pitchAngle = 0                          #Initial Pitch
-        self.rollAngle = 0                           #Initial Roll
+        # Values
+        # Initial Pitch
+        self.pitchAngle = 0
+        # Initial Roll
+        self.rollAngle = 0
         
     def setAngle(self, pitch, roll):
         self.pitchAngle = pitch*self.unit*self.pitchPadding
         self.rollAngle = roll
         self.repaint()
         
-    def paintEvent(self,event):
-        painterPFD = qtg.QPainter(self)
-        painterPFD.translate(self.width()/2,self.height()/2)
-        
-        
-        #Colors
-        blue = qtg.QColor(0, 64, 128, 255)
-        brown = qtg.QColor(120, 64, 0, 255)
-        #Pen
+    def paintEvent(self, event):
+        painter = qtg.QPainter(self)
+        painter.translate(self.width() / 2, self.height() / 2)
+        painter.setRenderHints(qtg.QPainter.HighQualityAntialiasing, True)
+
+        # Pen
         pen = qtg.QPen()
         pen.setWidthF(self.unit/2)
         pen.setColor(qtc.Qt.white)
         pen.setJoinStyle(qtc.Qt.MiterJoin)
-        painterPFD.setPen(pen)
-        #Brush
+        painter.setPen(pen)
+        # Brush
         brush = qtg.QBrush()
         brush.setStyle(qtc.Qt.SolidPattern)
         brush.setColor(qtc.Qt.transparent)
-        painterPFD.setBrush(brush)
-        
-        
-        #Horizon Indicator Begin
-        painterPFD.save()
+        painter.setBrush(brush)
+
+        self.paint_horizon_indicator(painter, pen, brush)
+        self.paint_pitch_indicator(painter, pen)
+        self.paint_roll_indicator(painter, pen, brush)
+        self.paint_aircraft_indicator(painter, pen)
+
+    def paint_horizon_indicator(self, painter, pen, brush):
+        painter.save()
         pen.setColor(qtc.Qt.transparent)
-        painterPFD.setPen(pen)
-        painterPFD.rotate(-self.rollAngle)
-        painterPFD.translate(0,self.pitchAngle)
+        painter.setPen(pen)
+        painter.rotate(-self.rollAngle)
+        painter.translate(0, self.pitchAngle)
         x = -self.unit*300
         w = self.unit*600
         h = self.unit*3000
-        sky = qtc.QRectF(x,-self.unit*3000,w,h)
-        ground = qtc.QRectF(x,0,w,h)
-        brush.setColor(blue)
-        painterPFD.setBrush(brush)
-        painterPFD.drawRect(sky)
-        brush.setColor(brown)
-        painterPFD.setBrush(brush)
-        painterPFD.drawRect(ground)
-        painterPFD.restore()
-        #Horizon Indicator End
-        
-        
-        #Pitch Indicator Begin
-        painterPFD.save()
-        pen.setColor(qtc.Qt.white)
-        painterPFD.setPen(pen)
-        painterPFD.setRenderHints(qtg.QPainter.HighQualityAntialiasing,
-                                       qtg.QPainter.TextAntialiasing)
-        painterPFD.rotate(-self.rollAngle)
-        painterPFD.translate(0,self.pitchAngle)
-        #Pitch Horizon Line
-        painterPFD.drawLine(-self.unit*300,0,self.unit*300,0)
-        #Pitch Clipper
+
+        sky = qtc.QRectF(x, -h, w, h)
+        brush.setColor(self.blue)
+        painter.setBrush(brush)
+        painter.drawRect(sky)
+
+        ground = qtc.QRectF(x, 0, w, h)
+        brush.setColor(self.brown)
+        painter.setBrush(brush)
+        painter.drawRect(ground)
+        painter.restore()
+
+    def paint_pitch_indicator(self, painter, pen):
+        painter.save()
+        pen.setColor(self.white)
+        painter.setPen(pen)
+        painter.rotate(-self.rollAngle)
+        painter.translate(0, self.pitchAngle)
+        # Pitch Horizon Line
+        painter.drawLine(-self.unit * 300, 0, self.unit * 300, 0)
+        # Pitch Clipper
         r = self.unit*43*self.rollScale             #Radius
         clipPathCircle = qtc.QRectF(-r,-self.pitchAngle-r,2*r,2*r)
         clipPath = qtg.QPainterPath()
         clipPath.addEllipse(clipPathCircle)
-        painterPFD.setClipPath(clipPath)
-        #Pitch Circle
+        painter.setClipPath(clipPath)
+        # Pitch Circle
         circle = qtc.QRectF(-self.unit*2,-self.unit*2,self.unit*4,self.unit*4)
-        painterPFD.drawEllipse(circle)
-        #Pitch Angle Lines
+        painter.drawEllipse(circle)
+        # Pitch Angle Lines
         pitchMain = np.linspace(10,90,num=9)
         pitchHalf = np.linspace(5,85,num=9)
         pitchQuarter = np.linspace(2.5,92.5,num=19)
         for p in pitchMain:
             x = self.unit*13                        #x-Position
             y = p*self.unit*self.pitchPadding       #y-Position
-            painterPFD.drawLine(-x,y,x,y)           #Upper Lines
-            painterPFD.drawLine(-x,-y,x,-y)         #Lower Lines
+            painter.drawLine(-x, y, x, y)           #Upper Lines
+            painter.drawLine(-x, -y, x, -y)         #Lower Lines
             w = self.unit*10                        #Width
             h = self.unit*6                         #Height
             s = str(int(p))                         #Pitch Angle
             a = qtc.Qt.AlignCenter                  #Alignment
-            painterPFD.drawText(qtc.QRectF(-x-w,y-h/2,w,h),a,s)
-            painterPFD.drawText(qtc.QRectF(-x-w,-y-h/2,w,h),a,s)
-            painterPFD.drawText(qtc.QRectF(x,y-h/2,w,h),a,s)
-            painterPFD.drawText(qtc.QRectF(x,-y-h/2,w,h),a,s)
+            painter.drawText(qtc.QRectF(-x - w, y - h / 2, w, h), a, s)
+            painter.drawText(qtc.QRectF(-x - w, -y - h / 2, w, h), a, s)
+            painter.drawText(qtc.QRectF(x, y - h / 2, w, h), a, s)
+            painter.drawText(qtc.QRectF(x, -y - h / 2, w, h), a, s)
         for p in pitchHalf:
             x = self.unit*6.5
             y = p*self.unit*self.pitchPadding
-            painterPFD.drawLine(-x,y,x,y)           #Upper Lines
-            painterPFD.drawLine(-x,-y,x,-y)         #Lower Lines
+            painter.drawLine(-x, y, x, y)           #Upper Lines
+            painter.drawLine(-x, -y, x, -y)         #Lower Lines
         for p in pitchQuarter:
             x = self.unit*2.5
             y = p*self.unit*self.pitchPadding
-            painterPFD.drawLine(-x,y,x,y)           #Upper Lines
-            painterPFD.drawLine(-x,-y,x,-y)         #Lower Lines
-        painterPFD.restore()
-        #Pitch Indicator End
-        
-        
-        #Roll Indicator Begin
-        painterPFD.save()
-        painterPFD.scale(self.rollScale,self.rollScale)
+            painter.drawLine(-x, y, x, y)           #Upper Lines
+            painter.drawLine(-x, -y, x, -y)         #Lower Lines
+        painter.restore()
+
+    def paint_roll_indicator(self, painter, pen, brush):
+        painter.save()
+        painter.scale(self.rollScale, self.rollScale)
         pen.setWidthF((self.unit/2)/self.rollScale)
-        painterPFD.setPen(pen)
-        painterPFD.setRenderHints(qtg.QPainter.HighQualityAntialiasing)       
-        #Roll Indicator Dynamic Triangle
-        painterPFD.save()
-        painterPFD.rotate(-self.rollAngle)
-        triangleDynamic = (qtc.QPointF(-self.unit*2.25,-self.unit*40),
-                           qtc.QPointF(self.unit*2.25,-self.unit*40),
-                           qtc.QPointF(self.unit*0,-self.unit*44))
-        triangleDynamicPoly = qtg.QPolygonF(triangleDynamic)
-        painterPFD.drawPolygon(triangleDynamicPoly)
-        painterPFD.restore()
-        #Roll Indicator Static Triangle
-        brush.setColor(qtc.Qt.white)
-        painterPFD.setBrush(brush)
-        triangleStatic = (qtc.QPointF(-self.unit*1.75,-self.unit*47.5),
-                          qtc.QPointF(self.unit*1.75,-self.unit*47.5),
-                          qtc.QPointF(self.unit*0,-self.unit*44.5))
-        triangleStaticPoly = qtg.QPolygonF(triangleStatic)
-        painterPFD.drawPolygon(triangleStaticPoly)
-        #Roll Indicator Static Strokes
-        smallStrokes = (-45,-20,-10,10,20,45)
-        for s in smallStrokes:
-            painterPFD.save()
-            painterPFD.rotate(s)
-            painterPFD.drawLine(0,-self.unit*47.5,0,-self.unit*44.5)
-            painterPFD.restore()
-        bigStrokes = (-60,-30,30,60)
-        for b in bigStrokes:
-            painterPFD.save()
-            painterPFD.rotate(b)
-            painterPFD.drawLine(0,-self.unit*50.5,0,-self.unit*44.5)
-            painterPFD.restore()
-        painterPFD.restore()
-        #Roll Indicator End
-        
-        
-        #Aircraft Indicator Begin
-        painterPFD.save()
-        painterPFD.scale(self.aircraftScale,self.aircraftScale)
+        painter.setPen(pen)
+
+        # Roll Indicator Dynamic Triangle
+        painter.save()
+        painter.rotate(-self.rollAngle)
+        painter.drawPolygon(self.roll_indicator_triangle('dyn'))
+        painter.restore()
+
+        # Roll Indicator Static Triangle
+        brush.setColor(self.white)
+        painter.setBrush(brush)
+        painter.drawPolygon(self.roll_indicator_triangle('sta'))
+
+        # Roll Indicator Static Strokes
+        small_strokes = (-45, -20, -10, 10, 20, 45)
+        for s in small_strokes:
+            painter.save()
+            painter.rotate(s)
+            painter.drawLine(0, -self.unit * 47.5, 0, -self.unit * 44.5)
+            painter.restore()
+        big_strokes = (-60, -30, 30, 60)
+        for b in big_strokes:
+            painter.save()
+            painter.rotate(b)
+            painter.drawLine(0, -self.unit * 50.5, 0, -self.unit * 44.5)
+            painter.restore()
+        painter.restore()
+
+    def roll_indicator_triangle(self, state):
+        n = {'dyn': {'w': 2.25, 'c': 44, 'a': 40},
+             'sta': {'w': 1.75, 'c': 44.5, 'a': 47.5}}
+        triangle = (qtc.QPointF(-self.unit * 2.25, -self.unit * n[state]['a']),
+                    qtc.QPointF(self.unit* 2.25, -self.unit * n[state]['a']),
+                    qtc.QPointF(0, -self.unit * n[state]['c']))
+        triangle_polygon = qtg.QPolygonF(triangle)
+        return triangle_polygon
+
+    def paint_aircraft_indicator(self, painter, pen):
+        painter.save()
+        painter.scale(self.aircraftScale, self.aircraftScale)
         pen.setWidthF(self.unit*3)
-        painterPFD.setPen(pen)
-        #Aircraft Indicator Left Wing
-        lWingPath = qtg.QPainterPath(qtc.QPointF(-self.unit*45,0))
-        lWingPath.lineTo(qtc.QPointF(-self.unit*15,0))
-        lWingPath.lineTo(qtc.QPointF(-self.unit*15,self.unit*5))
-        #Aircraft Indicator Right Wing
-        rWingPath = qtg.QPainterPath(qtc.QPointF(self.unit*45,0))
-        rWingPath.lineTo(qtc.QPointF(self.unit*15,0))
-        rWingPath.lineTo(qtc.QPointF(self.unit*15,self.unit*5))
-        #Aircraft Indicator Center Box
-        painterPFD.drawPath(lWingPath)
-        painterPFD.drawPath(rWingPath)
-        painterPFD.drawPoint(qtc.QPointF(0,0))
-        painterPFD.restore()
-        #Aircraft Indicator End
+        painter.setPen(pen)
+        # Wings
+        painter.drawPath(self.wing_path('l'))
+        painter.drawPath(self.wing_path('r'))
+        # Center Box
+        painter.drawPoint(qtc.QPointF(0, 0))
+        painter.restore()
+
+    def wing_path(self, side):
+        vz = {'l': -1, 'r': +1}
+        wing_path = qtg.QPainterPath(qtc.QPointF(vz[side]*self.unit * 45, 0))
+        wing_path.lineTo(qtc.QPointF(vz[side]*self.unit * 15, 0))
+        wing_path.lineTo(qtc.QPointF(vz[side]*self.unit * 15, self.unit * 5))
+        return wing_path
--- src/gaugeIndicator.py
+++ src/gaugeIndicator.py
@@ -11,21 +11,22 @@ import numpy as np
 class Gauge(qtw.QWidget):
     def __init__(self, size, maxValue, minValue=0, *args, **kwargs):
         super(Gauge, self).__init__(*args, **kwargs)
-        if size<75:size=75
-        self.setFixedSize(1.5*size,size)
+        if size < 75:
+            size = 75
+        self.setFixedSize(int(1.5*size),size)
         self.unit = self.height()/100
         self.maxAngle = 210                        #Max Angle of Indication
-        #Font
+        # Font
         self.font = qtg.QFont("Courier New")
         self.font.setWeight(61)
-        self.font.setPixelSize(17.5*self.unit)
+        self.font.setPixelSize(int(17.5*self.unit))
         self.setFont(self.font)
-        #Background
+        # Background
         self.setAutoFillBackground(True)
         palette = qtg.QPalette()
         palette.setColor(self.backgroundRole(), qtc.Qt.black)
         self.setPalette(palette)
-        #Values
+        # Values
         self.value = 0
         self.maxValue = maxValue
         self.minValue = minValue
@@ -42,41 +43,38 @@ class Gauge(qtw.QWidget):
     def paintEvent(self,event):
         painterGauge = qtg.QPainter(self)
         painterGauge.translate(self.width()/2,self.height()/2)
-        painterGauge.setRenderHints(qtg.QPainter.HighQualityAntialiasing,
-                                    qtg.QPainter.TextAntialiasing)
-        
-        
-        #Pen
+        painterGauge.setRenderHints(qtg.QPainter.HighQualityAntialiasing, True)
+
+        # Pen
         pen = qtg.QPen()
         pen.setWidthF(2*self.unit)
         pen.setColor(qtc.Qt.white)
         pen.setJoinStyle(qtc.Qt.MiterJoin)
         painterGauge.setPen(pen)
-        #Brush
+        # Brush
         brush = qtg.QBrush()
         brush.setStyle(qtc.Qt.SolidPattern)
         brush.setColor(qtc.Qt.transparent)
-                
-        
-        #Radial Indicator Begin
+
+        # Radial Indicator Begin
         painterGauge.save()
         painterGauge.scale(0.7,0.7)
         h = self.height()
         r = (h/2)*0.9
-        #Radial Indicator Arc
+        # Radial Indicator Arc
         pen.setWidthF(self.unit*4)
         pen.setCapStyle(qtc.Qt.FlatCap)
         painterGauge.setPen(pen)
         arcRect = qtc.QRectF(-h/2,-h/2,h,h)
         painterGauge.drawArc(arcRect,0,-self.maxAngle*16)
-        #Radial Indicator Pie
+        # Radial Indicator Pie
         brush.setColor(qtc.Qt.gray)
         painterGauge.setBrush(brush)
         pen.setColor(qtc.Qt.transparent)
         painterGauge.setPen(pen)
         pieRect = qtc.QRectF(-r,-r,2*r,2*r)
         painterGauge.drawPie(pieRect,0,-self.angle*16)      #Degree in 1/16
-        #Radial Max Stroke
+        # Radial Max Stroke
         pen.setColor(qtc.Qt.red)
         pen.setCapStyle(qtc.Qt.SquareCap)
         painterGauge.setPen(pen)
@@ -85,7 +83,7 @@ class Gauge(qtw.QWidget):
         painterGauge.drawLine(-adj*h/2,-opp*h/2,
                               -adj*(1.2*h/2),
                               -opp*(1.2*h/2))
-        #Radial Indicator Bar
+        # Radial Indicator Bar
         painterGauge.save()
         painterGauge.rotate(self.angle)
         pen.setColor(qtc.Qt.white)
@@ -94,10 +92,9 @@ class Gauge(qtw.QWidget):
         painterGauge.drawLine(0,0,r-self.unit,0)
         painterGauge.restore()
         painterGauge.restore()
-        #Radial Indicator End
-        
-        
-        #Box Indicator Begin
+        # Radial Indicator End
+
+        # Box Indicator Begin
         painterGauge.save()
         yBox = -0.275*h
         hBox = 0.225*h
@@ -106,4 +103,4 @@ class Gauge(qtw.QWidget):
         textRect = qtc.QRectF(0.05,yBox,0.6*h,hBox)
         painterGauge.drawText(textRect,0x0082,str(round(100*self.angle/self.maxAngle,1)))
         painterGauge.restore()
-        #Box Indicator End
+        # Box Indicator End
